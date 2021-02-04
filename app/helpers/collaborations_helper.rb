99341b1c999fd0d220507ac8c0fda28076dd4070
asrivastava <asrivastava@C02DRR78MD6M.local>
2021-02-05 02:37:31 +0530

    Collaboration Helper Tested

--- app/helpers/collaborations_helper.rb
+++ app/helpers/collaborations_helper.rb
@@ -1,9 +1,9 @@
 module CollaborationsHelper
 
-  def check_collaboration(challenge_id, owner_id )
-    if owner_id == current_user.id
+  def check_collaboration(challenge_id, owner_id, current_user_id )
+    if owner_id == current_user_id
       link_to image_tag('contribute.png'), collaborate_url(:challenge_id => challenge_id, :user_id => owner_id ), :class => 'owner-like'
-    elsif Collaboration.check(challenge_id, current_user.id) < 1
+    elsif Collaboration.check(challenge_id, current_user_id) < 1
       link_to image_tag('contribute.png'), collaborate_url( :challenge_id => challenge_id, :user_id => owner_id )
     else
       link_to image_tag('contributed.png'), remove_collaboration_url(:challenge_id => challenge_id, :user_id => owner_id )
--- spec/helpers/collaborations_helper_spec.rb
+++ spec/helpers/collaborations_helper_spec.rb
@@ -1,12 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe CollaborationsHelper, type: :helper do
-  let!(:user) { FactoryBot.create(:user) }
-  let!(:challenge) { FactoryBot.create(:challenge, user: user) }
-  let!(:collaboration) { FactoryBot.create(:collaboration, user: user, challenge: challenge) }
-  context "from Find Collaborations" do
-      it "returns Collaborators" do
-        expect( helper.find_collaborations(challenge.id).count).to be > 0
+
+  context "from Check Collaborations" do
+    let!(:user) { FactoryBot.create(:user) }
+    let!(:challenge) { FactoryBot.create(:challenge) }
+    let!(:collaboration) { FactoryBot.create(:collaboration, user: user, challenge: challenge) }
+      it "displays faded icon for challenge owner" do
+        expect( helper.check_collaboration(challenge.id, user.id, user.id) ).to have_selector(".owner-like")
+        expect( helper.check_collaboration(challenge.id, user.id, user.id) ).to include(collaborate_url)
       end
+
+      it "displays collaborated icon for challenge" do
+        expect( helper.check_collaboration(challenge.id, challenge.user_id, user.id) ).to include(remove_collaboration_url)
+      end
+  end
+
+  context "from Check Collaborations" do
+    let!(:user) { FactoryBot.create(:user) }
+    let!(:challenge) { FactoryBot.create(:challenge) }
+    it "displays not collaborated icon for challenge" do
+      expect( helper.check_collaboration(challenge.id, challenge.user_id, user.id) ).to include(collaborate_url)
+    end
   end
 end
